be20b9c4622914ee211792ceb226a2c00a96438c
romain2518 <rolenoir25@gmail.com>
2022-07-20 14:57:52 +0200

    Add Volunteering type, reception structure and thematic fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,32 +3,78 @@
 namespace App\DataFixtures;
 
 use App\Entity\Message;
+use App\Entity\ReceptionStructure;
+use App\Entity\Thematic;
 use App\Entity\User;
+use App\Entity\VolunteeringType;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(SluggerInterface $slugger) {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
 
+        //? Providers
         $roles = [
             'ROLE_USER',
             'ROLE_MODERATOR',
             'ROLE_ADMIN'
         ];
 
+        $volunteeringTypeList = [
+            'Public',
+            'Private',
+            'Pair to pair',
+            'Other'
+        ];
+
+        $receptionStructureList = [
+            'Association',
+            'Local community',
+            'Other'
+        ];
+
+        $thematicList = [
+            'Sustainable development',
+            'Animation',
+            'Education',
+            'Cultural',
+            'Animal cause',
+            'Animal care',
+            'Social',
+            'Health',
+            'Prevent discriminations',
+            'Women rights',
+            'Migrant /refugee support',
+            'Construction',
+            'Agriculture, permaculture',
+            'Sport',
+            'Digital',
+            'Seniors, intergenerational links',
+            'Disability'
+        ];
+
+        //? Object lists
         $users = [];
+        $volunteeringTypes = [];
+        $receptionStructures = [];
+        $thematics = [];
         
         //! User
         for ($i=1; $i <= 30; $i++) { 
             $user = new User();
 
             $user->setPseudo('Member #' . $i);
-            $user->setPseudoSlug('member_' . $i);
+            $user->setPseudoSlug($this->slugger->slug($user->getPseudo()));
             $user->setRoles([$roles[array_rand($roles)]]);
             $user->setPassword('$2y$13$H1YPtVq4xMwKhd1H1D817OOnHRnYklL.3ZmM/ujTL28n9o/43w8MW'); // Password : 1234
             $user->setFirstname($faker->firstName());
@@ -64,6 +110,38 @@ class AppFixtures extends Fixture
         }
 
 
+        //! Volunteering type
+        foreach ($volunteeringTypeList as $volunteeringTypeName) {
+            $volunteeringType = new VolunteeringType();
+
+            $volunteeringType->setName($volunteeringTypeName);
+            $volunteeringType->setSlugName($this->slugger->slug($volunteeringType->getName()));
+
+            $volunteeringTypes[] = $volunteeringType;
+            $manager->persist($volunteeringType);
+        }
+
+        //! Reception structure
+        foreach ($receptionStructureList as $receptionStructureName) {
+            $receptionStructure = new ReceptionStructure();
+
+            $receptionStructure->setName($receptionStructureName);
+            $receptionStructure->setSlugName($this->slugger->slug($receptionStructure->getName()));
+
+            $receptionStructures[] = $receptionStructure;
+            $manager->persist($receptionStructure);
+        }
+
+        //! Thematic
+        foreach ($thematicList as $thematicName) {
+            $thematic = new Thematic();
+
+            $thematic->setName($thematicName);
+            $thematic->setSlugName($this->slugger->slug($thematic->getName()));
+
+            $thematics[] = $thematic;
+            $manager->persist($thematic);
+        }
 
         // $product = new Product();
         // $manager->persist($product);
