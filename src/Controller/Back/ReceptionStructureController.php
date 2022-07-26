aa297670ffad79137d00b92fa9eaf277b9ed1761
romain2518 <rolenoir25@gmail.com>
2022-07-26 09:30:50 +0200

    Change routes

--- src/Controller/Back/ReceptionStructureController.php
+++ src/Controller/Back/ReceptionStructureController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/back/reception/structure")
+ * @Route("/back/receptionStructure")
  */
 class ReceptionStructureController extends AbstractController
 {
