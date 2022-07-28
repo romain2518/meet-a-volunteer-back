fcd1e3b7b43cac1b4927da4a27617de3d88659ab
romain2518 <rolenoir25@gmail.com>
2022-07-28 11:48:48 +0200

    Updated the JWT Response by adding connected user id to the payload

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+	/**
+	 * @var RequestStack
+	 */
+	private $requestStack;
+
+    /**
+	 * @var UserRepository
+	 */
+	private $userRepo;
+
+	/**
+	 * @param RequestStack $requestStack
+	 */
+	public function __construct(RequestStack $requestStack, UserRepository $userRepo)
+	{
+		$this->requestStack = $requestStack;
+		$this->userRepo = $userRepo;
+	}
+
+	/**
+	 * @param JWTCreatedEvent $event
+	 *
+	 * @return void
+	 */
+	public function onJWTCreated(JWTCreatedEvent $event)
+	{
+        $user = $event->getUser();
+        $userByEntity = $this->userRepo->findOneBy(['pseudo' => $user->getUserIdentifier()]);
+        
+
+		$payload = $event->getData();
+        $payload['id'] = $userByEntity->getId();
+
+		$event->setData($payload);
+
+		$header = $event->getHeader();
+		$header['cty'] = 'JWT';
+
+		$event->setHeader($header);
+	}
+}
